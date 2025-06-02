4d86e105cf8beb5bd5b088d1ba25238514971c6c
YC酱luyancib <2058282593@qq.com>
2025-06-02 20:46:41 +0800

    Update BottomBarDestination.kt

--- manager/app/src/main/java/com/sukisu/ultra/ui/screen/BottomBarDestination.kt
+++ manager/app/src/main/java/com/sukisu/ultra/ui/screen/BottomBarDestination.kt
@@ -21,9 +21,9 @@ enum class BottomBarDestination(
     val iconNotSelected: ImageVector,
     val rootRequired: Boolean,
 ) {
-    Home(HomeScreenDestination, R.string.home, Icons.Filled.Home, Icons.Outlined.Home, false),
     Kpm(KpmScreenDestination, R.string.kpm_title, Icons.Filled.Archive, Icons.Outlined.Archive, true),
     SuperUser(SuperUserScreenDestination, R.string.superuser, Icons.Filled.AdminPanelSettings, Icons.Outlined.AdminPanelSettings, true),
+    Home(HomeScreenDestination, R.string.home, Icons.Filled.Home, Icons.Outlined.Home, false),
     Module(ModuleScreenDestination, R.string.module, Icons.Filled.Extension, Icons.Outlined.Extension, true),
     Settings(SettingScreenDestination, R.string.settings, Icons.Filled.Settings, Icons.Outlined.Settings, false),
 }
